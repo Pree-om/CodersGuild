9fb830e445ec2d66da9b0c9efe9250cd61fbc099
PREE OM <ompree29@gmail.com>
2026-03-11 20:02:29 +0530

    Implement minOperations to make array increasing

---
+++ MinOpsToMakeTheArrayIncreasing.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int moves = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] <= nums[i-1]){
+                int needed = nums[i-1] + 1;
+                moves += needed - nums[i];
+                nums[i] = needed;
+            }
+        }
+        return moves;
+    }
+}
